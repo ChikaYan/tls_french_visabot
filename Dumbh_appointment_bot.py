c1158d90d588a82dfd7784f2c23670ba0158070c
Walter Wu <waltertianhaowu@meta.com>
2026-04-24 14:56:28 +0100

    update

--- Dumbh_appointment_bot.py
+++ Dumbh_appointment_bot.py
@@ -264,7 +264,7 @@ def create_driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
-    driver = uc.Chrome(options=options, version_main=None)
+    driver = uc.Chrome(options=options, version_main=147)
     print("  Chrome started.")
     return driver
 
